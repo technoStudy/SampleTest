ca179e3e0ed36cab17b9ae81490ce0185109a417
daulet2030 <daulet2030@gmail.com>
2021-03-09 06:29:35 +0600

    Initial commit

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,17 @@
+import org.testng.annotations.Test;
+
+public class SimpleTest {
+    @Test
+    public void test1() {
+        System.out.println("Success");
+    }
+    @Test
+    public void test2() {
+        System.out.println("Success");
+    }
+    @Test
+    public void test3() {
+        System.out.println("Success");
+    }
+
+}
